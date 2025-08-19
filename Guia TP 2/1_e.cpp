19102307c58f795687249bfcfb7c7b34d1b9f4b8
rafagorr06 <rafaelgorrochategui@gmail.com>
2025-08-19 20:10:35 -0300

    GTP2-1_e

---
+++ Guia TP 2/1_e.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main() {
+	list<int>L = {1,3,4,2,5,6};
+	
+	list<int>::iterator p,q;
+	p = L.begin();
+	q = ++p;
+	p = L.erase(q);
+	p++;
+	q = p;
+	q++;
+	cout<<"p="<<*p<<" ";
+	cout<<"q="<<*q;
+	return 0;
+}
+
+
+
+
+
+
+
